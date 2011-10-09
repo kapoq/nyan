1e5b17b97511d72ce4406815f8baf7a9b3d92a67
dave@kapoq.com <dave@kapoq.com>
2011-10-09 14:03:08 +0200

    Add some options to Flight

--- lib/nyan/flight.rb
+++ lib/nyan/flight.rb
@@ -1,10 +1,10 @@
 module Nyan
   class Flight
-    def initialize
-      @stage   = Stage.new
-      
+    def initialize(options = {})
+      @framerate = options.fetch(:framerate, 0.09)
+            
+      @stage    = Stage.new(options.fetch(:stage, {}))
       @rainbow  = Sprite.new(:x => 0, :y => 17, :z => 1, :frames => Nyan::Rainbow.to_frames)
-
       @cat_head = Sprite.new(:x        => Nyan::Rainbow::TOTAL_LENGTH - 2,
                             :y         => 17,
                             :z         => 100,
@@ -19,11 +19,15 @@ module Nyan
                               when [:offset, :initial]  then @x -= 2
                               end
                             end)
-
       @pop_tart = Sprite.new(:x => @cat_head.x - 20,
                             :y => @cat_head.y - 2,
                             :z => @cat_head.z - 1,
                             :frames => Nyan::PopTart.to_frames)
+
+      @line = Sprite.new(:x => 0, :y => 0, :z => 10, :frames => %w(1111111111))
+      @dot  = Sprite.new(:x => 5, :y => 0, :z => 11, :frames => %w(2))
+      @stage.add_sprite(@line)
+      @stage.add_sprite(@dot)
       
       @stage.add_sprite(@rainbow)
       @stage.add_sprite(@cat_head)
@@ -34,7 +38,7 @@ module Nyan
     def next!
       maybe_add_a_twinkly_star
       @stage.play!
-      while Time.now - @tick_time < 0.09; end
+      while Time.now - @tick_time < @framerate; end
       @tick_time = Time.now
     end
 
